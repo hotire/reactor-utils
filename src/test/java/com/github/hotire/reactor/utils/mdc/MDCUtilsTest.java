42e9bd3a148fdd58dfe4dbc8c87ccf04f25110f3
user <gngh0101@gmail.com>
2020-02-16 12:36:55 +0900

    Verify constructor of MDCUtils

---
+++ src/test/java/com/github/hotire/reactor/utils/mdc/MDCUtilsTest.java
@@ -0,0 +1,29 @@
+package com.github.hotire.reactor.utils.mdc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import org.junit.jupiter.api.Test;
+
+
+class MDCUtilsTest {
+
+  @Test
+  void create()
+    // given
+    throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    final Constructor<MDCUtils> constructor = MDCUtils.class.getDeclaredConstructor(null);
+
+    // when then
+    assertThrows(IllegalAccessException.class, constructor::newInstance);
+
+    // when
+    constructor.setAccessible(true);
+    final MDCUtils instance = constructor.newInstance();
+
+    // then
+    assertThat(instance).isNotNull();
+  }
+}
