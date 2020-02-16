b0d9ce73a092035da27f025215ceaa65f8eeee0a
user <gngh0101@gmail.com>
2020-02-16 22:26:45 +0900

    Use MDC(runnable, callable) instead of origin

--- src/main/java/com/github/hotire/reactor/utils/mdc/MDCScheduledExecutorServiceDecorator.java
+++ src/main/java/com/github/hotire/reactor/utils/mdc/MDCScheduledExecutorServiceDecorator.java
@@ -1,7 +1,10 @@
 package com.github.hotire.reactor.utils.mdc;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -14,31 +17,53 @@ import java.util.concurrent.TimeoutException;
 public class MDCScheduledExecutorServiceDecorator implements ScheduledExecutorService {
 
   private final ScheduledExecutorService delegate;
+  private final Runnable doOnExecute;
+  private final Runnable doOnTerminate;
 
   public MDCScheduledExecutorServiceDecorator(ScheduledExecutorService delegate) {
     this.delegate = delegate;
+    final Map<String, String> copyContextMap = MDCUtils.getContextMap();
+    this.doOnExecute = () -> copyContextMap.forEach(MDCUtils::put);
+    this.doOnTerminate = () -> copyContextMap.forEach((key, value) -> MDCUtils.remove(key));
+
+  }
+
+  protected Runnable decorateRunnable(Runnable runnable) {
+    return new MDCRunnable(runnable, doOnExecute, doOnTerminate);
+  }
+
+  protected <T> Callable<T> decorateCallable(Callable<T> callable) {
+    return new MDCCallable<>(callable, doOnExecute, doOnExecute);
+  }
+
+  protected <T> Collection<? extends Callable<T>> wrapCallableCollection(
+    Collection<? extends Callable<T>> tasks) {
+    return tasks.stream()
+                .filter(task -> !(task instanceof MDCCallable))
+                .map(this::decorateCallable)
+                .collect(toList());
   }
 
   @Override
   public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
-    return getDelegate().schedule(command, delay, unit);
+    return getDelegate().schedule(decorateRunnable(command), delay, unit);
   }
 
   @Override
   public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
-    return getDelegate().schedule(callable, delay, unit);
+    return getDelegate().schedule(decorateCallable(callable), delay, unit);
   }
 
   @Override
   public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period,
     TimeUnit unit) {
-    return getDelegate().scheduleAtFixedRate(command, initialDelay, period, unit);
+    return getDelegate().scheduleAtFixedRate(decorateRunnable(command), initialDelay, period, unit);
   }
 
   @Override
   public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay,
     TimeUnit unit) {
-    return getDelegate().scheduleAtFixedRate(command, initialDelay, delay, unit);
+    return getDelegate().scheduleAtFixedRate(decorateRunnable(command), initialDelay, delay, unit);
   }
 
   @Override
@@ -68,46 +93,46 @@ public class MDCScheduledExecutorServiceDecorator implements ScheduledExecutorSe
 
   @Override
   public <T> Future<T> submit(Callable<T> task) {
-    return getDelegate().submit(task);
+    return getDelegate().submit(decorateCallable(task));
   }
 
   @Override
   public <T> Future<T> submit(Runnable task, T result) {
-    return getDelegate().submit(task, result);
+    return getDelegate().submit(decorateRunnable(task), result);
   }
 
   @Override
   public Future<?> submit(Runnable task) {
-    return getDelegate().submit(task);
+    return getDelegate().submit(decorateRunnable(task));
   }
 
   @Override
   public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks)
     throws InterruptedException {
-    return getDelegate().invokeAll(tasks);
+    return getDelegate().invokeAll(wrapCallableCollection(tasks));
   }
 
   @Override
   public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout,
     TimeUnit unit) throws InterruptedException {
-    return getDelegate().invokeAll(tasks, timeout, unit);
+    return getDelegate().invokeAll(wrapCallableCollection(tasks), timeout, unit);
   }
 
   @Override
   public <T> T invokeAny(Collection<? extends Callable<T>> tasks)
     throws InterruptedException, ExecutionException {
-    return getDelegate().invokeAny(tasks);
+    return getDelegate().invokeAny(wrapCallableCollection(tasks));
   }
 
   @Override
   public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
     throws InterruptedException, ExecutionException, TimeoutException {
-    return getDelegate().invokeAny(tasks, timeout, unit);
+    return getDelegate().invokeAny(wrapCallableCollection(tasks), timeout, unit);
   }
 
   @Override
   public void execute(Runnable command) {
-    getDelegate().execute(command);
+    getDelegate().execute(decorateRunnable(command));
   }
 
   protected ScheduledExecutorService getDelegate() {
