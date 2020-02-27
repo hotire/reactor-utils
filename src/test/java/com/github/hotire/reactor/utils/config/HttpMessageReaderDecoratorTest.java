1ea9a9297a1e60db4b18c803c1f6842f2cf2591d
hotire <gngh0101@gmail.com>
2020-02-28 00:34:24 +0900

    Verify getReadableMediaTypes method in HttpMessageReaderDecorator

--- src/test/java/com/github/hotire/reactor/utils/config/HttpMessageReaderDecoratorTest.java
+++ src/test/java/com/github/hotire/reactor/utils/config/HttpMessageReaderDecoratorTest.java
@@ -1,10 +1,14 @@
 package com.github.hotire.reactor.utils.config;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.core.ResolvableType;
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.HttpMessageReader;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class HttpMessageReaderDecoratorTest {
 
@@ -23,10 +27,32 @@ class HttpMessageReaderDecoratorTest {
 
     @Test
     void getReadableMediaTypes() {
+        // given
+        final List<MediaType> mediaTypes = mock(List.class);
+        final HttpMessageReader<?> reader = mock(HttpMessageReader.class);
+        final HttpMessageReaderDecorator<?> decorator = new HttpMessageReaderDecorator<>(reader);
+
+        // when
+        when(reader.getReadableMediaTypes()).thenReturn(mediaTypes);
+        final List<MediaType> result = decorator.getReadableMediaTypes();
+
+        // then
+        assertThat(result).isEqualTo(mediaTypes);
     }
 
     @Test
     void canRead() {
+        // given
+        final boolean expected = true;
+        final HttpMessageReader<?> reader = mock(HttpMessageReader.class);
+        final HttpMessageReaderDecorator<?> decorator = new HttpMessageReaderDecorator<>(reader);
+
+        // when
+        when(reader.canRead(any(), any())).thenReturn(expected);
+        final boolean result = decorator.canRead(mock(ResolvableType.class), mock(MediaType.class));
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
