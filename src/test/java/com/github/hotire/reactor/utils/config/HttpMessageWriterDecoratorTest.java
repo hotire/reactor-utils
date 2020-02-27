a4ebab8bdc3f4fb285cfd628954c5849c4c3e14d
hotire <gngh0101@gmail.com>
2020-02-28 01:19:42 +0900

    Verify write method in HttpMessageWriterDecorator

--- src/test/java/com/github/hotire/reactor/utils/config/HttpMessageWriterDecoratorTest.java
+++ src/test/java/com/github/hotire/reactor/utils/config/HttpMessageWriterDecoratorTest.java
@@ -3,10 +3,13 @@ package com.github.hotire.reactor.utils.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.HttpMessageWriter;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -43,9 +46,32 @@ class HttpMessageWriterDecoratorTest {
 
     @Test
     void canWrite() {
+        // given
+        final boolean expected = true;
+        final HttpMessageWriter<?> reader = mock(HttpMessageWriter.class);
+        final HttpMessageWriterDecorator<?> decorator = new HttpMessageWriterDecorator<>(reader);
+
+        // when
+        when(reader.canWrite(any(), any())).thenReturn(expected);
+        final boolean result = decorator.canWrite(any(), any());
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     void write() {
+        // given
+        final Mono expected = mock(Mono.class);
+        final HttpMessageWriter<?> reader = mock(HttpMessageWriter.class);
+        final HttpMessageWriterDecorator<?> decorator = new HttpMessageWriterDecorator<>(reader);
+
+        // when
+        when(reader.write(any(), any(), any(), any(), anyMap())).thenReturn(expected);
+        final Mono<?> result = decorator.write(any(), any(), any(), any(), anyMap());
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
