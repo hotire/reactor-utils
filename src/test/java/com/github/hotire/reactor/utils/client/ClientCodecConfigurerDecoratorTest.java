c606d0b3145755379da987f0cb8d5ba2e8e57f22
hotire <gngh0101@gmail.com>
2020-03-01 01:37:04 +0900

    Verify customCodecs method in ClientCodecConfigurerDecorator

--- src/test/java/com/github/hotire/reactor/utils/client/ClientCodecConfigurerDecoratorTest.java
+++ src/test/java/com/github/hotire/reactor/utils/client/ClientCodecConfigurerDecoratorTest.java
@@ -2,9 +2,10 @@ package com.github.hotire.reactor.utils.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.codec.ClientCodecConfigurer;
+import org.springframework.http.codec.CodecConfigurer;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class ClientCodecConfigurerDecoratorTest {
 
@@ -23,10 +24,32 @@ class ClientCodecConfigurerDecoratorTest {
 
     @Test
     void defaultCodecs() {
+        // given
+        final ClientCodecConfigurer clientCodecConfigurer = mock(ClientCodecConfigurer.class);
+        final ClientCodecConfigurerDecorator decorator = new ClientCodecConfigurerDecorator(clientCodecConfigurer);
+
+        // when
+        when(clientCodecConfigurer.defaultCodecs()).thenReturn(mock(ClientCodecConfigurer.ClientDefaultCodecs.class));
+        decorator.defaultCodecs();
+
+        // then
+        verify(clientCodecConfigurer, times(1)).defaultCodecs();
     }
 
     @Test
     void customCodecs() {
+        // given
+        final CodecConfigurer.CustomCodecs customCodecs = mock(ClientCodecConfigurer.CustomCodecs.class);
+        final ClientCodecConfigurer clientCodecConfigurer = mock(ClientCodecConfigurer.class);
+        final ClientCodecConfigurerDecorator decorator = new ClientCodecConfigurerDecorator(clientCodecConfigurer);
+
+        // when
+        when(clientCodecConfigurer.customCodecs()).thenReturn(customCodecs);
+        final CodecConfigurer.CustomCodecs result = decorator.customCodecs();
+
+        // then
+        assertThat(result).isEqualTo(customCodecs);
+        verify(clientCodecConfigurer, times(1)).customCodecs();
     }
 
     @Test
