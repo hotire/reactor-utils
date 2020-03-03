1fefdd16f09813b6af61f2d8086d00e73c56afa0
hotire <gngh0101@gmail.com>
2020-03-03 22:18:37 +0900

    Use application_json_utf8 instead of application_json

--- src/main/java/com/github/hotire/reactor/utils/codec/Utf8HttpMessageWriterDecorator.java
+++ src/main/java/com/github/hotire/reactor/utils/codec/Utf8HttpMessageWriterDecorator.java
@@ -1,5 +1,6 @@
 package com.github.hotire.reactor.utils.codec;
 
+import com.github.hotire.reactor.utils.http.MediaTypeAdapter;
 import org.reactivestreams.Publisher;
 import org.springframework.core.ResolvableType;
 import org.springframework.http.MediaType;
@@ -14,8 +15,18 @@ public class Utf8HttpMessageWriterDecorator<T> extends HttpMessageWriterDecorato
         super(delegate);
     }
 
+    private final MediaTypeAdapter adapter = MediaTypeAdapter.of(MediaType.APPLICATION_JSON);
+
     @Override
-    public Mono<Void> write(final Publisher<? extends T> publisher, final ResolvableType resolvableType, final MediaType mediaType, final ReactiveHttpOutputMessage reactiveHttpOutputMessage, final Map<String, Object> hints) {
+    public Mono<Void> write(final Publisher<? extends T> publisher, final ResolvableType resolvableType, MediaType mediaType, final ReactiveHttpOutputMessage reactiveHttpOutputMessage, final Map<String, Object> hints) {
+        if (adapter.equals(mediaType)) {
+            mediaType = MediaType.APPLICATION_JSON_UTF8;
+        }
+
+        if (adapter.equals(reactiveHttpOutputMessage.getHeaders().getContentType())) {
+            reactiveHttpOutputMessage.getHeaders().setContentType(MediaType.APPLICATION_JSON_UTF8);
+        }
+
         return getDelegate().write(publisher, resolvableType, mediaType, reactiveHttpOutputMessage, hints);
     }
 }
