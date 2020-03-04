e79ac5c64dbd1b4ad26d3175082bdc1087557d28
hotire <gngh0101@gmail.com>
2020-03-05 00:54:15 +0900

    Use method instead field

--- src/main/java/com/github/hotire/reactor/utils/codec/Utf8HttpMessageWriterDecorator.java
+++ src/main/java/com/github/hotire/reactor/utils/codec/Utf8HttpMessageWriterDecorator.java
@@ -11,20 +11,26 @@ import reactor.core.publisher.Mono;
 import java.util.Map;
 
 public class Utf8HttpMessageWriterDecorator<T> extends HttpMessageWriterDecorator<T>{
+
+    private final MediaTypeAdapter adapter;
+
     public Utf8HttpMessageWriterDecorator(HttpMessageWriter<T> delegate) {
         super(delegate);
+        adapter = MediaTypeAdapter.of(MediaType.APPLICATION_JSON);
     }
 
-    private final MediaTypeAdapter adapter = MediaTypeAdapter.of(MediaType.APPLICATION_JSON);
+    public MediaTypeAdapter getAdapter() {
+        return adapter;
+    }
 
     @SuppressWarnings("deprecation")
     @Override
     public Mono<Void> write(final Publisher<? extends T> publisher, final ResolvableType resolvableType, MediaType mediaType, final ReactiveHttpOutputMessage reactiveHttpOutputMessage, final Map<String, Object> hints) {
-        if (adapter.equals(mediaType)) {
+        if (getAdapter().equals(mediaType)) {
             mediaType = MediaType.APPLICATION_JSON_UTF8;
         }
 
-        if (adapter.equals(reactiveHttpOutputMessage.getHeaders().getContentType())) {
+        if (getAdapter().equals(reactiveHttpOutputMessage.getHeaders().getContentType())) {
             reactiveHttpOutputMessage.getHeaders().setContentType(MediaType.APPLICATION_JSON_UTF8);
         }
 
