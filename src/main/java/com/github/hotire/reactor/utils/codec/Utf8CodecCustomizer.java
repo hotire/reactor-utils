1f4f9f1a0d958b581838772a6bf96239a4eb7e56
hotire <gngh0101@gmail.com>
2020-03-05 01:10:27 +0900

    Add Utf8CodecCustomizer

---
+++ src/main/java/com/github/hotire/reactor/utils/codec/Utf8CodecCustomizer.java
@@ -0,0 +1,11 @@
+package com.github.hotire.reactor.utils.codec;
+
+import org.springframework.boot.web.codec.CodecCustomizer;
+import org.springframework.http.codec.CodecConfigurer;
+
+public class Utf8CodecCustomizer implements CodecCustomizer {
+    @Override
+    public void customize(CodecConfigurer configurer) {
+        configurer.getWriters().forEach(writer -> configurer.customCodecs().register(new Utf8HttpMessageWriterDecorator<>(writer)));
+    }
+}
