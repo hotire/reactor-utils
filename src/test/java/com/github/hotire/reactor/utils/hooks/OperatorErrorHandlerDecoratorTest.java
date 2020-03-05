fce100a244aedfb4427f8391d3cee4e41df88603
hotire <gngh0101@gmail.com>
2020-03-06 01:23:06 +0900

    Verify apply method

--- src/test/java/com/github/hotire/reactor/utils/hooks/OperatorErrorHandlerDecoratorTest.java
+++ src/test/java/com/github/hotire/reactor/utils/hooks/OperatorErrorHandlerDecoratorTest.java
@@ -3,7 +3,7 @@ package com.github.hotire.reactor.utils.hooks;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class OperatorErrorHandlerDecoratorTest {
 
@@ -18,5 +18,23 @@ class OperatorErrorHandlerDecoratorTest {
 
         // then
         assertThat(result).isEqualTo(errorHandler);
+    }
+
+    @Test
+    void apply() {
+        // given
+        final Throwable throwable = mock(Throwable.class);
+        final Object obj = mock(Object.class);
+
+        final OperatorErrorHandler errorHandler = mock(OperatorErrorHandler.class);
+        final OperatorErrorHandlerDecorator decorator = new OperatorErrorHandlerDecorator(errorHandler);
+
+        // when
+        when(errorHandler.apply(throwable, obj)).thenReturn(throwable);
+        final Throwable result = decorator.apply(throwable, obj);
+
+        // then
+        assertThat(result).isEqualTo(throwable);
+        verify(errorHandler, times(1)).apply(throwable, obj);
     }
 }
