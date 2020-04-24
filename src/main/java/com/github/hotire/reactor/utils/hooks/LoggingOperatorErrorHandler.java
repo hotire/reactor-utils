afa96569d82114e0c3c48616117f67988bbcddec
hotire <gngh0101@gmail.com>
2020-04-25 01:07:23 +0900

    Fix logging format

--- src/main/java/com/github/hotire/reactor/utils/hooks/LoggingOperatorErrorHandler.java
+++ src/main/java/com/github/hotire/reactor/utils/hooks/LoggingOperatorErrorHandler.java
@@ -4,12 +4,13 @@ package com.github.hotire.reactor.utils.hooks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class LoggingOperatorErrorHandler implements OperatorErrorHandler {
     private static final Logger log = LoggerFactory.getLogger(LoggingOperatorErrorHandler.class);
 
     @Override
-    public Throwable apply(Throwable throwable, Object o) {
-        log.error("arg : {}", o);
-        return throwable;
+    public Throwable apply(Throwable error, Object arg) {
+        log.error("error : {}, arg : {}", error, arg);
+        return error;
     }
 }
