0d89ee5cadb10ef99be0ffd4070bc7a9d11511d0
hotire <gngh0101@gmail.com>
2020-05-04 02:40:03 +0900

    Add FunctionalInterface to OperatorErrorHandler

--- src/main/java/com/github/hotire/reactor/utils/hooks/OperatorErrorHandler.java
+++ src/main/java/com/github/hotire/reactor/utils/hooks/OperatorErrorHandler.java
@@ -2,5 +2,6 @@ package com.github.hotire.reactor.utils.hooks;
 
 import java.util.function.BiFunction;
 
+@FunctionalInterface
 public interface OperatorErrorHandler extends BiFunction<Throwable, Object, Throwable> {
 }
