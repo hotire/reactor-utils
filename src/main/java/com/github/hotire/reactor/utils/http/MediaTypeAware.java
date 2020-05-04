25647cb9a01d8068d02c59e3472e4cb83c7051f5
hotire <gngh0101@gmail.com>
2020-05-05 00:33:23 +0900

    Add FunctionalInterface to MediaTypeAware

--- src/main/java/com/github/hotire/reactor/utils/http/MediaTypeAware.java
+++ src/main/java/com/github/hotire/reactor/utils/http/MediaTypeAware.java
@@ -2,6 +2,7 @@ package com.github.hotire.reactor.utils.http;
 
 import org.springframework.http.MediaType;
 
+@FunctionalInterface
 public interface MediaTypeAware {
     MediaType getMediaType();
 }
