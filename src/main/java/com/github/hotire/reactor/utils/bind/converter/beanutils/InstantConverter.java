f3b77bd426738ad59bcd66c116fb6ae743f40345
hotire <gngh0101@gmail.com>
2020-05-08 00:28:33 +0900

    Add InstantConverter

---
+++ src/main/java/com/github/hotire/reactor/utils/bind/converter/beanutils/InstantConverter.java
@@ -0,0 +1,10 @@
+package com.github.hotire.reactor.utils.bind.converter.beanutils;
+
+import org.apache.commons.beanutils.Converter;
+
+public class InstantConverter implements Converter {
+    @Override
+    public <T> T convert(Class<T> aClass, Object o) {
+        return null;
+    }
+}
