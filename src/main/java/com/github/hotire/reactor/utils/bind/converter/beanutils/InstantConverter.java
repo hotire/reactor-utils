2d7578a61cef769439640ed6315c517d15ca4b8b
hotire <gngh0101@gmail.com>
2020-05-10 00:54:44 +0900

    opt

--- src/main/java/com/github/hotire/reactor/utils/bind/converter/beanutils/InstantConverter.java
+++ src/main/java/com/github/hotire/reactor/utils/bind/converter/beanutils/InstantConverter.java
@@ -5,6 +5,7 @@ import org.apache.commons.beanutils.Converter;
 import java.time.Instant;
 
 public class InstantConverter implements Converter {
+
     @Override
     public <T> T convert(final Class<T> type, final Object value) {
         return type.cast(Instant.parse((String)value));
