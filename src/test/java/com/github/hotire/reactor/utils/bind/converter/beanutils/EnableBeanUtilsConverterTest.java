dd459faf21078ecc032eca1e6643f77e555c4073
hotire <gngh0101@gmail.com>
2020-05-17 10:47:06 +0900

    Add TODO in  EnableBeanUtilsConverter

---
+++ src/test/java/com/github/hotire/reactor/utils/bind/converter/beanutils/EnableBeanUtilsConverterTest.java
@@ -0,0 +1,15 @@
+package com.github.hotire.reactor.utils.bind.converter.beanutils;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class EnableBeanUtilsConverterTest {
+
+    @DisplayName("Scan")
+    @Disabled
+    @Test
+    void scan() {
+        // TODO EnableBeanUtils Scan
+    }
+}
