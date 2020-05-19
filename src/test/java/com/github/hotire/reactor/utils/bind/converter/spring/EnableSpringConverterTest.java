602e590daf52226835bddf1f40a1e54933c8a50f
hotire <gngh0101@gmail.com>
2020-05-20 03:15:12 +0900

    Add TDOO

---
+++ src/test/java/com/github/hotire/reactor/utils/bind/converter/spring/EnableSpringConverterTest.java
@@ -0,0 +1,15 @@
+package com.github.hotire.reactor.utils.bind.converter.spring;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EnableSpringConverterTest {
+
+    @DisplayName("Scan")
+    @Disabled
+    @Test
+    void scan() {
+        // TODO Scan
+    }
+}
