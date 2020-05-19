2bfb9d6279cee1a3208b0a0e2818a96b891b6faf
hotire <gngh0101@gmail.com>
2020-05-19 15:35:28 +0900

    Verfiy scan in EnableConverter

---
+++ src/test/java/com/github/hotire/reactor/utils/bind/converter/EnableConverterTest.java
@@ -0,0 +1,15 @@
+package com.github.hotire.reactor.utils.bind.converter;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EnableConverterTest {
+
+    @DisplayName("Scan")
+    @Disabled
+    @Test
+    void scan() {
+        // TODO EnableBeanUtils Scan
+    }
+}
