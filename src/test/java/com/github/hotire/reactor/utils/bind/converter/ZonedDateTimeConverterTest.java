0b521ce27a635aa03fe63207aa6c0b4aa7a1b44a
hotire <gngh0101@gmail.com>
2020-05-26 00:17:48 +0900

    Verify convert

--- src/test/java/com/github/hotire/reactor/utils/bind/converter/ZonedDateTimeConverterTest.java
+++ src/test/java/com/github/hotire/reactor/utils/bind/converter/ZonedDateTimeConverterTest.java
@@ -1,7 +1,22 @@
 package com.github.hotire.reactor.utils.bind.converter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ZonedDateTimeConverterTest {
+    @Test
+    void convert() {
+        // given
+        final String expected = "2002-06-18T20:30+09:00[Asia/Seoul]";
+        final ZonedDateTimeConverter converter = new ZonedDateTimeConverter();
 
+        // when
+        final ZonedDateTime result = converter.convert(ZonedDateTime.class, expected);
+
+        // then
+        assertThat(result.toString()).isEqualTo(expected);
+    }
 }
