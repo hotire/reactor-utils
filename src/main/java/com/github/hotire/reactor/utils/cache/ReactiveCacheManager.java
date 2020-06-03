db8a199eb78621c4693a60acde0d66b306a5bb83
hotire <gngh0101@gmail.com>
2020-06-04 00:55:49 +0900

    Add cacheFlux

--- src/main/java/com/github/hotire/reactor/utils/cache/ReactiveCacheManager.java
+++ src/main/java/com/github/hotire/reactor/utils/cache/ReactiveCacheManager.java
@@ -2,12 +2,17 @@ package com.github.hotire.reactor.utils.cache;
 
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import reactor.cache.CacheFlux;
 import reactor.cache.CacheMono;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Signal;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
+
+import static java.util.stream.Collectors.toList;
 
 public class ReactiveCacheManager {
 
@@ -17,7 +22,7 @@ public class ReactiveCacheManager {
         this.cacheManager = cacheManager;
     }
 
-    public <T> Mono<T> findCachedMono(final String cacheName, final Object key, final Supplier<Mono<T>> retriever, final Class<T> classType) {
+    public <T> Mono<T> cacheMono(final String cacheName, final Object key, final Supplier<Mono<T>> retriever, final Class<T> classType) {
         return CacheMono
                 .lookup(k -> Mono.justOrEmpty(getCache(cacheName).get(k, classType)).map(Signal::next), key)
                 .onCacheMissResume(Mono.defer(retriever))
@@ -27,8 +32,21 @@ public class ReactiveCacheManager {
                                                  .thenEmpty(Mono.empty()));
     }
 
-    public <T> Mono<T> findCachedMono(final String cacheName, final Object key, final Class<T> classType) {
-        return findCachedMono(cacheName, key, () -> null, classType);
+    public <T> Mono<T> cacheMono(final String cacheName, final Object key, final Class<T> classType) {
+        return cacheMono(cacheName, key, () -> null, classType);
+    }
+
+    public <T> Flux<T> cacheFlux(final String cacheName, final Object key, final Supplier<Flux<T>> retriever, final Class<T> classType) {
+        return CacheFlux
+                .lookup(k -> Mono.justOrEmpty(getCache(cacheName).get(k, classType))
+                             .map(list -> (List<T>)list)
+                             .map(list -> list.stream()
+                                              .map(Signal::next)
+                                              .collect(toList())), key)
+                .onCacheMissResume(Flux.defer(retriever))
+                .andWriteWith((k, signalList) -> Mono.just(signalList)
+                                                     .doOnNext(list -> getCache(cacheName).put(k, list))
+                                                     .then());
     }
 
     protected Cache getCache(final String name) {
---
+++ src/test/java/com/github/hotire/reactor/utils/cache/ReactiveCacheAspectTest.java
@@ -0,0 +1,46 @@
+package com.github.hotire.reactor.utils.cache;
+
+import org.assertj.core.util.Maps;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.expression.MapAccessor;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.ParserContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReactiveCacheAspectTest {
+
+    @Test
+    void spel() {
+        final String message = "my foo is #name, i bar #age";
+        ExpressionParser parser = new SpelExpressionParser();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "hotire");
+        map.put("age", "1");
+        StandardEvaluationContext context = new StandardEvaluationContext(map);
+//        context.addPropertyAccessor(new MapAccessor());
+        context.setVariable("name", "hotire");
+        context.setVariable("age", "1");
+//        Expression expression = parser.parseExpression(message, ParserContext.TEMPLATE_EXPRESSION);
+//        String value = expression.getValue(context, String.class);
+        String result = parser.parseExpression(message).getValue(context, String.class);
+        System.out.println(result);
+    }
+
+    @Test
+    void parse() {
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp = parser.parseExpression("my foo is #name");
+
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        context.setVariable("name", "hotire");
+        String result = exp.getValue(context, String.class);
+        System.out.println(result);
+    }
+}
