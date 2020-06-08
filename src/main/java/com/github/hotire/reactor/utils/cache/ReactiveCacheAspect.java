3755ad072672aac1845300a67d569b42c8b2dcb1
hotire <gngh0101@gmail.com>
2020-06-09 00:46:06 +0900

    Implment ReactiveCacheAspect

--- src/main/java/com/github/hotire/reactor/utils/cache/ReactiveCacheAspect.java
+++ src/main/java/com/github/hotire/reactor/utils/cache/ReactiveCacheAspect.java
@@ -1,30 +1,29 @@
 package com.github.hotire.reactor.utils.cache;
 
+import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.ResolvableType;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Aspect
+@RequiredArgsConstructor
 public class ReactiveCacheAspect {
 
     private final ReactiveCacheManager reactiveCacheManager;
-
-    private LocalVariableTableParameterNameDiscoverer discoverer = new LocalVariableTableParameterNameDiscoverer();
-
-
-    public ReactiveCacheAspect(ReactiveCacheManager reactiveCacheManager) {
-        this.reactiveCacheManager = reactiveCacheManager;
-    }
 
     @SuppressWarnings("unchecked")
     @Around("@annotation(reactiveCacheable)")
@@ -32,7 +31,19 @@ public class ReactiveCacheAspect {
         final MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         final String key = parseSpel(methodSignature.getParameterNames(), joinPoint.getArgs(), reactiveCacheable.key());
 
-        return reactiveCacheManager.cacheMono(reactiveCacheable.name(), key, methodSignature.getReturnType());
+        final Method method = methodSignature.getMethod();
+        final ParameterizedType parameterizedType = (ParameterizedType) method.getGenericReturnType();
+        final Type returnTypeInsideMono = parameterizedType.getActualTypeArguments()[0];
+        final Class<?> returnClass = ResolvableType.forType(returnTypeInsideMono).resolve();
+        final Supplier retriever = () -> {
+            try {
+                return joinPoint.proceed();
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        return reactiveCacheManager.cacheMono(reactiveCacheable.name(), key, retriever, returnClass);
     }
 
 
@@ -44,7 +55,6 @@ public class ReactiveCacheAspect {
 
         return new SpelExpressionParser().parseExpression(spel).getValue(context, String.class);
     }
-
 
     protected String generateKey(final Object... objects) {
         return Arrays.stream(objects)
