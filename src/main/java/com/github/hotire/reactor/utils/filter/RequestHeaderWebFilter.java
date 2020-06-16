3bc80b4f7f04b60eebfa3c6eba5cfc71e8505887
hotire <gngh0101@gmail.com>
2020-06-17 01:07:27 +0900

    Implment HandlerFilterFunction of WebfluxFilter

--- src/main/java/com/github/hotire/reactor/utils/filter/RequestHeaderWebFilter.java
+++ src/main/java/com/github/hotire/reactor/utils/filter/RequestHeaderWebFilter.java
@@ -1,19 +1,30 @@
 package com.github.hotire.reactor.utils.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 @Slf4j
-public class RequestHeaderWebFilter implements WebFilter {
+public class RequestHeaderWebFilter implements WebfluxFilter {
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+    public Mono<Void> filter(final ServerWebExchange exchange, final WebFilterChain chain) {
         exchange.getRequest()
                 .getHeaders()
                 .forEach((key, value) -> log.debug("key : [{}] / value : {}", key, value));
         return chain.filter(exchange);
+    }
+
+    @Override
+    public Mono<ServerResponse> filter(final ServerRequest serverRequest, final HandlerFunction<ServerResponse> handlerFunction) {
+        serverRequest.exchange()
+                     .getRequest()
+                     .getHeaders()
+                     .forEach((key, value) -> log.debug("key : [{}] / value : {}", key, value));
+        return handlerFunction.handle(serverRequest);
     }
 }
