caad3d0e37230b44ee7d84bba979982943facb79
hotire <gngh0101@gmail.com>
2020-06-21 00:42:24 +0900

    Verify generateKey

--- src/test/java/com/github/hotire/reactor/utils/cache/ReactiveCacheAspectTest.java
+++ src/test/java/com/github/hotire/reactor/utils/cache/ReactiveCacheAspectTest.java
@@ -1,44 +1,23 @@
 package com.github.hotire.reactor.utils.cache;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.expression.Expression;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
 
-import java.util.HashMap;
-import java.util.Map;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 class ReactiveCacheAspectTest {
 
-    @Disabled
     @Test
-    void spel() {
-        final String message = "my foo is #name, i bar #age";
-        ExpressionParser parser = new SpelExpressionParser();
-        Map<String, String> map = new HashMap<>();
-        map.put("name", "hotire");
-        map.put("age", "1");
-        StandardEvaluationContext context = new StandardEvaluationContext(map);
-//        context.addPropertyAccessor(new MapAccessor());
-        context.setVariable("name", "hotire");
-        context.setVariable("age", "1");
-//        Expression expression = parser.parseExpression(message, ParserContext.TEMPLATE_EXPRESSION);
-//        String value = expression.getValue(context, String.class);
-        String result = parser.parseExpression(message).getValue(context, String.class);
-        System.out.println(result);
-    }
+    void generateKey() {
+        // given
+        final String expected = "1:1:1";
+        final Object[] objects = new Object[]{"1", 1, 1L};
+        final ReactiveCacheAspect aspect = new ReactiveCacheAspect(mock(ReactiveCacheManager.class));
 
-    @Disabled
-    @Test
-    void parse() {
-        ExpressionParser parser = new SpelExpressionParser();
-        Expression exp = parser.parseExpression("my foo is #name");
+        // when
+        final String result = aspect.generateKey(objects);
 
-        StandardEvaluationContext context = new StandardEvaluationContext();
-        context.setVariable("name", "hotire");
-        String result = exp.getValue(context, String.class);
-        System.out.println(result);
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
