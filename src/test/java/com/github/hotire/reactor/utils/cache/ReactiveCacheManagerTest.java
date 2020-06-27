33dee3d8874646ce064dcd78cdb4f00cdb977e52
hotire <gngh0101@gmail.com>
2020-06-27 13:39:02 +0900

    Verify cacheMono

--- src/test/java/com/github/hotire/reactor/utils/cache/ReactiveCacheManagerTest.java
+++ src/test/java/com/github/hotire/reactor/utils/cache/ReactiveCacheManagerTest.java
@@ -4,12 +4,37 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ReactiveCacheManagerTest {
+
+    @Test
+    void cacheMono() {
+        // given
+        final String cacheName = "";
+        final String key = "";
+        final Object data = new Object();
+        final CacheManager cacheManager = mock(CacheManager.class);
+        final Cache cache = new ConcurrentMapCache(cacheName);
+        cache.put(key, data);
+        final ReactiveCacheManager manager = new ReactiveCacheManager(cacheManager,  mock(ObjectMapper.class));
+
+        // when
+        when(cacheManager.getCache(cacheName)).thenReturn(cache);
+        final Mono<Object> result = manager.cacheMono(cacheName, key, Object.class);
+
+        // then
+        StepVerifier.create(result)
+                    .expectNext(data)
+                    .verifyComplete();
+    }
 
     @Test
     void cast() {
